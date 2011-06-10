357d14f343c6d2dee5107d3f5795e05875129ccc
Jared Flatow <jflatow@gmail.com>
2011-06-09 18:50:51 -0700

    popdots: add google analytics

--- popdots/ui.js
+++ popdots/ui.js
@@ -262,4 +262,19 @@ $().ready(function () {
     $('#control form select').change(change_topic);
     $('#topic').focus();
     repeat(update, 60000);
+    install_analytics();
   });
+
+function install_analytics() {
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-2269620-5']);
+  _gaq.push(['_trackPageview']);
+  (function() {
+    var ga = document.createElement('script');
+    ga.type = 'text/javascript';
+    ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(ga, s);
+  })();
+}
