f51656eb2db8d82e1048e366365ee53b89f0ef61
Jared Flatow <jflatow@gmail.com>
2011-12-24 14:41:48 -0800

    html5: make multibox useful

--- html5/multibox.js
+++ html5/multibox.js
@@ -1,32 +1,53 @@
 (function ($) {
-  $.fn.multibox = function (datalist, attrs) {
+  var Keys = {
+    'backspace': 8,
+    'delete': 46,
+  };
+
+  $.fn.multibox = function (datalist, attrs, nresults, bind_arrows) {
     var defaults = {autocapitalize: 'off',
                     autocomplete: 'off',
                     autocorrect: 'off',
                     spellcheck: 'false',
                     multiple: 'multiple'};
-    var input = $('<input>').attr($.extend(defaults, attrs));
-    var output = $('<div>').css({opacity: 0}).hide();
+    var input = $('<input class="input">').attr($.extend(defaults, attrs));
+    var output = $('<div class="output">').css({opacity: .5});
+    var results = $('<div class="results">').css({opacity: 1});
+    var result = function (text, value) {
+      var html = '<span>' + text + '</span>';
+      return $('<button>' + html + '</button>').data('value', value)
+      .one('click', function () {
+          results.append($(this).text($(html).text())
+                         .click(function () { $(this).focus(); })
+                         .keypress(function (e) {
+                             if (e.keyCode == Keys.backspace || e.keyCode == Keys.delete) {
+                               var self = $(this), next = self.next(), prev = self.prev();
+                               self.remove();
+                               if (next.length)
+                                 next.focus();
+                               else if (prev.length)
+                                 prev.focus();
+                               else
+                                 input.focus();
+                             }
+                           }));
+          input.focus();
+        })
+    };
     input
-    .focus(function () {
-        input.keyup();
-        output.show();
-        output.animate({opacity: 1}, 5e2);
-      })
-    .blur(function () {
-        output.animate({opacity: 0}, 5e2, function () { output.hide() });
-      })
-    .keyup(function (e) {
+    .on('focus input', function (e) {
+        var ival = input.val();
         output.empty();
         $.each(datalist, function (i, data) {
-            var match = data.text.match(input.val());
-            if (match)
-              output.append($('<button>' + data.text.split(match[0]).join('<b>' + match[0] + '</b>') + '</button>')
-                            .click(function () {
-                                input.val(data.value);
-                              }));
+            if (output.children().length < (nresults || 10)) {
+              var match = data.text.match(new RegExp(ival, 'i'));
+              if (match)
+                output.append(result(data.text.split(match[0]).join('<b>' + match[0] + '</b>'), match[0]));
+            }
           });
+        if (ival)
+          output.append(result('&ldquo;<b>' + ival + '</b>&rdquo;', ival));
       });
-    return this.append(input, output);
+    return this.append(input, output, results);
   }
 })(jQuery);
